e26ebdbd51fccae8bb5048b8dc97cf6f5189db99
PastelSnowChild <n.lahteinen1@gmail.com>
2025-12-22 18:55:56 +0200

    app.jsx ei toiminut, takas yrittään part 2

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        // Put every top–level node_module into its own chunk.
-        // Example:  node_modules/@polar-sh/sdk → chunk “@polar-sh”
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            const name = id
-              .split("/node_modules/")[1] // strip leading path
-              .replace(/^@?([^/]+).*$/, "$1"); // top-level package name
-            return name;
-          }
-        },
-      },
-    },
-  },
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
+      target: "cloudflare-module"
     }),
   ],
 })
