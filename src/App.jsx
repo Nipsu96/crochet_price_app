4dc175bb39a238d1f4168cb8a7f9fe687a4537b9
PastelSnowChild <n.lahteinen1@gmail.com>
2025-12-22 16:46:15 +0200

    let's try this ver5

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css'
 import Price_form from './components/Price_form'
-import bg from "./assets/img/background";
+import bg from "./assets/img/background.jpg";
 
 function App() {
 
